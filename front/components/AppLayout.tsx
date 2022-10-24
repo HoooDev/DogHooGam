0c3c7634850f67ba1d112a5ed4d2fa7f19f5ee88
Oh haengsong <haengsong@naver.com>
2022-10-24 14:18:59 +0900

    feat: 상단바 레이아웃

--- front/components/AppLayout.tsx
+++ front/components/AppLayout.tsx
@@ -1,11 +1,17 @@
 import styles from "./AppLayout.module.scss";
+import Navbar from '../components/common/Navbar';
 
 type AppLayoutProps = {
   children: React.ReactNode;
 };
 
 const AppLayout = ({ children }: AppLayoutProps) => {
-  return <div className={`${styles.wrapper}`}>{children}</div>;
+  return <div className={`${styles.wrapper}`}>
+    <Navbar />
+    <div className={`${styles.children}`}>{children}</div>
+    
+    <footer>aaa</footer>
+    </div>;
 };
 
 export default AppLayout;
