8750de65c4e635a1403e3fecf10b39870f3ea113
이민재 <uiui96@naver.com>
2022-10-24 15:30:51 +0900

    springboot 환경설정, swagger 설정

---
+++ back/spring/src/main/java/com/c103/dog/config/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package com.c103.dog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket restAPI() {
+        Parameter parameterBuilder = new ParameterBuilder()
+                .name(HttpHeaders.AUTHORIZATION)
+                .description("Access Tocken")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build();
+
+        List<Parameter> globalParamters = new ArrayList<>();
+        globalParamters.add(parameterBuilder);
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(globalParamters)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.c103.dog"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("독후감")
+                .description("독후감's Swagger")
+                .version("0.0.1")
+                .build();
+    }
+}
---
+++ back/spring/src/test/java/com/c103/dog/DogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c103.dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
