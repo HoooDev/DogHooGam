ff42cd30827f7ca4d144bb77b291b1016385eb4a
이민재 <dlalswo9801@naver.com>
2022-10-28 03:47:18 +0900

    feat : 카카오 로그인 완료(서버 배포 후에 주소 변경 필요)

--- back/spring/src/main/java/com/c103/dog/api/service/UserServiceImpl.java
+++ back/spring/src/main/java/com/c103/dog/api/service/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.c103.dog.api.service;
 
 import com.c103.dog.DB.entity.User;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
     @Override
     public User getUserByUserId(String userId) {
--- back/spring/src/main/java/com/c103/dog/common/auth/SsafyUserDetails.java
+++ back/spring/src/main/java/com/c103/dog/common/auth/SsafyUserDetails.java
@@ -5,16 +5,15 @@ import com.c103.dog.DB.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
-public class SsafyUserDetails implements UserDetails {
-	
-	@Autowired
+public class SsafyUserDetails implements UserDetails , OAuth2User {
+
 	private User user;
-	
 
 	private Map<String, Object> attributes;
     
@@ -91,6 +90,8 @@ public class SsafyUserDetails implements UserDetails {
 		this.user = user;
 	}
 
+
+	@Override
 	public Map<String, Object> getAttributes() {
 		return attributes;
 	}
@@ -98,7 +99,9 @@ public class SsafyUserDetails implements UserDetails {
 	public void setAttributes(Map<String, Object> attributes) {
 		this.attributes = attributes;
 	}
-	
-	
 
+	@Override
+	public String getName() {
+		return attributes.get("sub").toString();
+	}
 }
