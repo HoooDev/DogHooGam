8967018c6d8bc19af1a6b9df150446e860371575
이민재 <uiui96@naver.com>
2022-10-27 13:39:24 +0900

    feat : findbyuserId 설정

--- back/spring/src/main/java/com/c103/dog/api/service/UserServiceImpl.java
+++ back/spring/src/main/java/com/c103/dog/api/service/UserServiceImpl.java
@@ -1,10 +1,16 @@
 package com.c103.dog.api.service;
 
 import com.c103.dog.DB.entity.User;
+import com.c103.dog.DB.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public User getUserByUserId(String userId) {
-        return null;
+        return userRepository.findByUserId(userId);
     }
 }
