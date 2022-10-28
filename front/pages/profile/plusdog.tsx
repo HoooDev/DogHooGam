d690bf5f0e504456eaefd5568fa83eb9883f4b26
이건후 <lgh3806@naver.com>
2022-10-28 13:22:50 +0900

    feat : plusDog객체 만듬

--- front/pages/profile/plusdog.tsx
+++ front/pages/profile/plusdog.tsx
@@ -1,12 +1,33 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import styles from "./plusdog.module.scss";
 import dogDummy from "../../public/icons/dogDummy.jpg";
+import defaultDog from "../../public/icons/defaultDog.svg";
 
 function Plusdog() {
   const walletRef = useRef<HTMLInputElement>(null);
   const walletAddress =
     "0xa06989ee6270d06b5f00e9a4b3374460276bf6e83edcbe432e4f509fcad061fe";
+  const [uploadimg, setUploadimg] = useState<any>(null);
+  const [nftDog, setNftDog] = useState({
+    dogName: "",
+    dogNumber: "",
+    dogSpecies: "",
+    dogBirth: "",
+    dogChar: ""
+  });
+
+  function handleImageUpload(e: any) {
+    const fileArr = e.target.files;
+    console.log(fileArr);
+    const file = fileArr[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      setUploadimg(reader.result);
+    };
+  }
 
   const getWalletAdd = () => {
     if (walletRef.current) {
@@ -16,56 +37,94 @@ function Plusdog() {
     }
   };
 
-  function maxLengthChk(object: EventTarget & HTMLInputElement) {
+  function maxLengthChk(object: any) {
+    setNftDog({ ...nftDog, dogBirth: `${object.value}년` });
+
     if (object.value.length > object.maxLength) {
       // eslint-disable-next-line no-param-reassign
       object.value = object.value.slice(0, object.maxLength);
     }
   }
-
+  console.log(nftDog);
   return (
     <div className={`${styles.plusDog}`}>
       <div className={`${styles.dogProfileBox}`}>
         <div className={`${styles.dogImgBox}`}>
-          <div className={`${styles.dogImg}`}>
-            <Image src={dogDummy} />
-          </div>
+          <input
+            className={`${styles.image}`}
+            onChange={(e) => handleImageUpload(e)}
+            id="uploadimg"
+            type="file"
+            accept="image/gif, image/jpeg, image/png"
+            hidden
+          />
+          <label className={`${styles.image}`} htmlFor="uploadimg">
+            {uploadimg ? (
+              <div className={`${styles.dogImg} `}>
+                <img className={`${styles.dogImg} `} src={uploadimg} alt="#" />
+              </div>
+            ) : (
+              <div className={`${styles.dogImg}`}>
+                <Image src={defaultDog} />
+              </div>
+            )}
+            {/* <div className={`${styles.dogImg}`}>
+              <Image src={dogDummy} />
+            </div> */}
+          </label>
         </div>
         <p className={`${styles.dogProfileTitle}  notoBold`}>강아지 프로필</p>
       </div>
       <div className={`${styles.dogInputForm} notoMid`}>
         <p className={`${styles.dogInputTitle}`}>강아지 이름</p>
-        <input type="text" className={`${styles.dogInput} notoReg`} /> <hr />
+        <input
+          type="text"
+          className={`${styles.dogInput} notoReg`}
+          onChange={(e) => {
+            setNftDog({ ...nftDog, dogName: e.target.value });
+          }}
+        />{" "}
+        <hr />
         <p className={`${styles.dogInputTitle}`}>등록번호</p>
-        <input type="number" className={`${styles.dogInput} notoReg`} /> <hr />
+        <input
+          type="number"
+          className={`${styles.dogInput} notoReg`}
+          onChange={(e) => {
+            setNftDog({ ...nftDog, dogNumber: e.target.value });
+          }}
+        />{" "}
+        <hr />
         <p className={`${styles.dogInputTitle}`}>견종</p>
-        <input type="text" className={`${styles.dogInput} notoReg`} /> <hr />
+        <input
+          type="text"
+          className={`${styles.dogInput} notoReg`}
+          onChange={(e) => {
+            setNftDog({ ...nftDog, dogSpecies: e.target.value });
+          }}
+        />{" "}
+        <hr />
         <p className={`${styles.dogInputTitle}`}>생년월일</p>
-        <div className={`${styles.dogBirthInputForm} flex notoReg`}>
+        <div className={`${styles.dogBirthInputForm} notoReg`}>
           <input
-            type="number"
+            type="date"
             placeholder="연도 ex) 2022"
             maxLength={4}
             className={`${styles.dogBirthInput} notoReg`}
-            onChange={(e) => maxLengthChk(e.target)}
+            onChange={(e) => {
+              setNftDog({ ...nftDog, dogBirth: e.target.value });
+            }}
           />
-          <input
-            type="number"
-            placeholder="월 ex) 1, ..., 12"
-            maxLength={2}
-            className={`${styles.dogBirthInput} notoReg`}
-            onChange={(e) => maxLengthChk(e.target)}
-          />
-          <input
-            type="number"
-            placeholder="일 ex) 1, ..., 31"
-            maxLength={2}
-            className={`${styles.dogBirthInput} notoReg`}
-            onChange={(e) => maxLengthChk(e.target)}
-          />
+          <hr />
         </div>
         <p className={`${styles.dogInputTitle}`}>성격</p>
-        <input type="text" className={`${styles.dogInput} notoReg`} /> <hr />
+        <input
+          type="text"
+          className={`${styles.dogInput} notoReg`}
+          onChange={(e) => {
+            setNftDog({ ...nftDog, dogChar: e.target.value });
+          }}
+        />{" "}
+        <hr />
         <div className={`${styles.walletTitleBox}`}>
           <p className={`${styles.dogInputTitle}`}>지갑주소</p>
           <button
