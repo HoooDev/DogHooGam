31b966fc57be23442b97a74efaa6545e133bc6fc
이동명 <aklsjd7380@naver.com>
2022-10-28 14:28:13 +0900

    fix:캘린더 수정

--- front/components/calendar/WalkRecord.tsx
+++ front/components/calendar/WalkRecord.tsx
@@ -1,14 +1,17 @@
-import Image from "next/image";
 import styles from "./WalkRecord.module.scss";
 
+import KakaoMap from "../walk/KakaoMap"; // 산책하기 카카오맵 일단 넣었습니당
+
 // 더미이미지
-import map from "../../public/image/map.png";
+// import map from "../../public/image/map.png";
 
 function WalkRecord() {
+  const dummy = { time: "00:34:27", dist: "3.27", coin: 5 };
+
   return (
     <div className={`${styles.wrapper}`}>
       <div className={`${styles.map}`}>
-        <Image src={map} alt="지도더미" width={360} height={400} />
+        <KakaoMap />
       </div>
       <div className={`${styles.bottom} flex`}>
         <div className={`${styles.sub1} notoBold fs-24`}>시간</div>
@@ -16,9 +19,9 @@ function WalkRecord() {
         <div className={`${styles.sub3} notoBold fs-24`}>획득 코인</div>
       </div>
       <div className={`${styles.content} flex`}>
-        <div className={`${styles.con1} notoMid fs-18`}>00 : 57 : 27</div>
-        <div className={`${styles.con2} notoMid fs-18`}>3.27 KM</div>
-        <div className={`${styles.con3} notoMid fs-18`}>5 코인</div>
+        <div className={`${styles.con1} notoMid fs-18`}>{dummy.time}</div>
+        <div className={`${styles.con2} notoMid fs-18`}>{dummy.dist} KM</div>
+        <div className={`${styles.con3} notoMid fs-18`}>{dummy.coin} 코인</div>
       </div>
     </div>
   );
