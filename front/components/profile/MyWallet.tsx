9b4f091b8f022b4a92f1d3557e96dcb2ded2dd84
이건후 <lgh3806@naver.com>
2022-10-28 17:53:43 +0900

    feat:마이페이지 지갑 추가 버튼 구현

--- front/components/profile/MyWallet.tsx
+++ front/components/profile/MyWallet.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import styles from "./MyWallet.module.scss";
 import walletLogo from "../../public/icons/walletLogo.png";
+import addImg from "../../public/icons/addImg.svg";
 
 function MyWallet() {
+  const dummy = false;
   const walletAddress =
     "0xa06989ee6270d06b5f00e9a4b3374460276bf6e83edcbe432e4f509fcad061fe";
   const handleCopyClipBoard = async (text: string) => {
@@ -21,22 +23,32 @@ function MyWallet() {
       <div className={`${styles.walletIcon}`}>
         <Image src={walletLogo} />
       </div>
-      <div className={`${styles.walletTextBox}`}>
-        <p className={`${styles.walletCoin}`}>보유코인 : 100 coin</p>
-        <div className={`${styles.walletAddressBox}`}>
-          <p className={`${styles.walletAddress1}`}>지갑주소 : </p>
-          <p className={`${styles.walletAddress2}`}>
-            0xa06989ee6270d06b5f00e9a4b3374460276bf6e83edcbe432e4f509fcad061fe
-            <button
-              type="button"
-              className={`${styles.copyBtn}`}
-              onClick={() => handleCopyClipBoard(walletAddress)}
-            >
-              복사
-            </button>
-          </p>
+      {dummy ? (
+        <div className={`${styles.walletTextBox}`}>
+          <p className={`${styles.walletCoin}`}>보유코인 : 100 coin</p>
+          <div className={`${styles.walletAddressBox}`}>
+            <p className={`${styles.walletAddress1}`}>지갑주소 : </p>
+            <p className={`${styles.walletAddress2}`}>
+              0xa06989ee6270d06b5f00e9a4b3374460276bf6e83edcbe432e4f509fcad061fe
+              <button
+                type="button"
+                className={`${styles.copyBtn}`}
+                onClick={() => handleCopyClipBoard(walletAddress)}
+              >
+                복사
+              </button>
+            </p>
+          </div>
         </div>
-      </div>
+      ) : (
+        // <div className={`${styles.walletTextBox}`}>
+        <div>
+          <div className={`${styles.addWalletBtn}`}>
+            <Image src={addImg} />
+          </div>
+          <p className={`${styles.addWalletBtnText}`}>지갑을 등록 해주세요!</p>
+        </div>
+      )}
     </div>
   );
 }
