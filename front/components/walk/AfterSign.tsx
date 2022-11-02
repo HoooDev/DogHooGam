59ee912682d653dc0b1b419936ecf6ffa160b105
윤정원 <wjd10577@naver.com>
2022-11-02 16:02:30 +0900

    feat : 산책페이지 타임워치 완성

--- front/components/walk/AfterSign.tsx
+++ front/components/walk/AfterSign.tsx
@@ -16,75 +16,41 @@ const AfterSign = () => {
   const { totalDist } = useSelector((state: RootState) => state.walk);
 
   const interval: { current: NodeJS.Timeout | null } = useRef(null);
-  const [timerTime, setTimerTime] = useState(0);
-  const [timerStart, setTimerStart] = useState(Date.now());
-  const [beforeTime, setBeforeTime] = useState(0);
 
-  const startTimer = () => {
-    if (interval.current) {
-      clearInterval(interval.current);
-    }
-    setTimerStart(Date.now());
-    interval.current = setInterval(() => {
-      setTimerTime(Date.now() - timerStart);
-    }, 10);
-  };
-
-  const restartTimer = () => {
-    if (interval.current) {
-      clearInterval(interval.current);
-    }
-    setBeforeTime(timerTime);
-    setTimerStart(Date.now());
-    interval.current = setInterval(() => {
-      setTimerTime(beforeTime + Date.now() - timerStart);
-    }, 100);
-  };
-
-  const stopTimer = () => {
-    if (interval.current) {
-      clearInterval(interval.current);
-    }
-  };
-
-  const resetTimer = () => {
-    setTimerStart(0);
-    setTimerTime(0);
-  };
-
-  const onPlayClick = () => {
-    restartTimer();
-    setIsPausing((prev) => !prev);
-  };
-
-  const onPuaseClick = () => {
-    if (interval.current) {
-      clearInterval(interval.current);
-    }
-    stopTimer();
-    setIsPausing((prev) => !prev);
-  };
-  const onStopClick = () => {
-    if (confirm("산책을 마치시겠습니까?")) {
-      dispatch(finishWalking(totalDist));
-      resetTimer();
-    }
-  };
-  const seconds = `0${(Math.floor(timerTime / 1000) % 60).toString(10)}`.slice(
-    -2
-  );
-  const minutes = `0${(Math.floor(timerTime / 60000) % 60).toString(10)}`.slice(
-    -2
-  );
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(true);
 
   useEffect(() => {
-    startTimer();
+    if (running) {
+      interval.current = setInterval(() => {
+        setTime((prev) => prev + 10);
+      }, 10);
+    } else if (!running) {
+      if (interval.current) {
+        clearInterval(interval.current);
+      }
+    }
     return () => {
       if (interval.current) {
         clearInterval(interval.current);
       }
     };
-  }, []);
+  }, [running]);
+
+  const onPlayClick = () => {
+    setRunning(true);
+    setIsPausing((prev) => !prev);
+  };
+
+  const onPuaseClick = () => {
+    setRunning(false);
+    setIsPausing((prev) => !prev);
+  };
+  const onStopClick = () => {
+    if (confirm("산책을 마치시겠습니까?")) {
+      dispatch(finishWalking(totalDist));
+    }
+  };
 
   return (
     <div className={`${styles.wrapper}`}>
@@ -99,7 +65,8 @@ const AfterSign = () => {
         </div>
         <div className={`${styles.time} flex column align-center`}>
           <div className={`${styles.time__num} fs-20`}>
-            {minutes} : {seconds}
+            {`0${(Math.floor(time / 60000) % 60).toString(10)}`.slice(-2)}:
+            {`0${(Math.floor(time / 1000) % 60).toString(10)}`.slice(-2)}
           </div>
           <div className={`${styles.time__unit} fs-12`}>시간(분)</div>
         </div>
