70fc5836f19da6da13576fde52d27f6d9b755bfe
seongdo Byun <dmdsns@naver.com>
2022-11-02 17:46:18 +0900

    fix: url수정

--- back/django/firstpjt/urls.py
+++ back/django/firstpjt/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat/', include('chatbots.urls')),
+    path('chatbot/', include('chatbots.urls')),
 ]
