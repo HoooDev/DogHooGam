f58aaea7072b38502cc69f401cac42d3a797820e
Oh haengsong <haengsong@naver.com>
2022-11-07 17:30:51 +0900

    fix : 임포트 에러수정

--- front/next.config.js
+++ front/next.config.js
@@ -6,10 +6,15 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [
+      path.join(__dirname, "styles"),
+      path.join(__dirname, "components/calendar/cale")
+    ],
     prependData: `
             @import "_variables.scss";
             @import "_utils.scss";
+            @import "DayCheck.scss";
+            @import "MakeCalendar.scss";
           `
   }
 };
