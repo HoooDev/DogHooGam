af30065393e5241881572d9d4ad802968a877b83
이동명 <aklsjd7380@naver.com>
2022-11-07 16:42:01 +0900

    feat : 메모 등록 개발

--- front/components/calendar/Todo.tsx
+++ front/components/calendar/Todo.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
 import axios from "axios";
-import {  useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { v4 } from "uuid";
 import styles from "./Todo.module.scss";
+
+import close from "../../public/icons/close.svg";
+import done from "../../public/icons/done.svg";
 
 function Todo() {
   const todoRef = useRef<HTMLInputElement>(null); // 새로 쓴 메모 값
@@ -10,17 +14,16 @@ function Todo() {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
-  const time = date.toLocaleDateString("ko-kr");
-  console.log(time);
-  console.log(date);
+  // const time = date.toLocaleDateString("ko-kr");
+  // console.log(time);
+
   const arr: string[] = ["산책하기", "씻기기", "예방접종", "밥먹기"];
   // const arr: string[] = [];
   console.log(arr);
 
-  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const onChange = (e: any) => {
     setText(e.target.value);
   };
-  console.log(text);
 
   const handleMessage = () => {
     if (text === "") {
@@ -33,14 +36,18 @@ function Todo() {
       headers: { Authorization: `Bearer ${Token}` },
       data: {
         content: text,
-        memoDate: time,
-        title: text
+        year,
+        month,
+        day
       }
     })
       .then((res) => {
         if (res.status === 200) {
           console.log("success");
           setText("");
+          if (todoRef.current?.value) {
+            todoRef.current.value = "";
+          }
         }
         return [];
       })
@@ -58,17 +65,19 @@ function Todo() {
     }
   };
   return (
-<<<<<<< HEAD
     <div className={`${styles.wrapper} flex`} id="메모">
-=======
-    <div className={`${styles.wrapper}`}>
->>>>>>> 16a4555ceb0c60080cee28fa2a37f83bcc02ae25
-      {arr.length !== 0 &&
-        arr.map((value: string): any => (
-          <div key={v4()} className={`${styles.list} notoBold fs-20`}>
-            {value}
-          </div>
-        ))}
+      <div>
+        {arr.length !== 0 &&
+          arr.map((value: string): any => (
+            <>
+              <div key={v4()} className={`${styles.list} notoBold fs-20`}>
+                {value}
+              </div>
+              <Image src={done} alt="완료" />
+              <Image src={close} alt="삭제" />
+            </>
+          ))}
+      </div>
       <input
         ref={todoRef}
         className={`${styles.new} notoBold fs-16`}
