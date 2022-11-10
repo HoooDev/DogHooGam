4061a1df00798611114ef7523ac57e162d3483be
윤정원 <wjd10577@naver.com>
2022-11-10 10:14:13 +0900

    feat : 산책 케로셀 기본 css 적용

--- front/components/walk/WalkSlider.jsx
+++ front/components/walk/WalkSlider.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 // import Image from "next/image";
 
 import styles from "./WalkSlider.module.scss";
