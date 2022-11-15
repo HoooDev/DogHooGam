ad952056441aef16926ef2aa0470d6a41d8a8d01
이민재 <uiui96@naver.com>
2022-11-15 17:34:33 +0900

    fix : msa 경로 fix

--- back/MSA/apigateway-service/src/main/java/com/cos/apigatewayservice/ApigatewayServiceApplication.java
+++ back/MSA/apigateway-service/src/main/java/com/cos/apigatewayservice/ApigatewayServiceApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class ApigatewayServiceApplication {
@@ -17,4 +20,20 @@ public class ApigatewayServiceApplication {
 	public HttpTraceRepository httpTraceRepository(){
 		return new InMemoryHttpTraceRepository();
 	}
+
+//	@Bean
+//	public CorsConfigurationSource corsConfigurationSource() {
+//		CorsConfiguration configuration = new CorsConfiguration();
+//		// configuration.addAllowedOrigin("*");
+//		configuration.addAllowedOriginPattern("*");
+//		configuration.addAllowedMethod("*");
+//		configuration.addAllowedHeader("*");
+//		//configuration.addExposedHeader(JwtTokenUtil.HEADER_STRING);
+//		configuration.setAllowCredentials(true);
+//		configuration.setMaxAge(3600L);
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/**", configuration);
+//		return source;
+//	}
+
 }
