230609d92f67617a65773a3eb4d281a9a70831eb
Oh haengsong <haengsong@naver.com>
2022-11-16 14:59:45 +0900

    feat: 피드 발행중 테스트

--- front/components/memory/MemoryList.tsx
+++ front/components/memory/MemoryList.tsx
@@ -2,16 +2,32 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { v4 } from "uuid";
+import { useSelector } from "react-redux";
 import styles from "./MemoryList.module.scss";
 import getFeed from "../../pages/api/feed/getFeed";
+import { RootState } from "../../redux/store";
 
 function MemoryList() {
   const [feeds, setFeeds] = useState<any>([]);
+  const isLoading = useSelector((state: RootState) => state.calendar.isLoading);
   useEffect(() => {
     getFeed()
       .then((res) => {
         if (res.status === 200) {
           console.log(res.data);
+          const memoryList = res.data;
+          if (isLoading) {
+            memoryList.push({
+              content: "발행중입니다.",
+              createDate: "2022-11-16 14:22",
+              feedImg: "발행중",
+              hide: false,
+              lat: 35.2059392,
+              lng: 126.81216,
+              pk: 9999,
+              transactionHash: "##"
+            });
+          }
           setFeeds(res.data.reverse());
         }
         return [];
@@ -34,6 +50,20 @@ function MemoryList() {
   return (
     <div className={`${styles.wrapper}`}>
       {feeds.map((feed: { pk: any; feedId: any; feedImg: any }) => {
+        if (feed.pk === 9999) {
+          return (
+            <div className={`${styles.item}`} key={v4()}>
+              <div>
+                <div
+                  className={`${styles.imgBox} flex column justify-center align-center`}
+                >
+                  <h1 className="notoBold fs-14">새로운 피드를</h1>
+                  <h1 className="notoBold fs-14">발행 중입니다.</h1>
+                </div>
+              </div>
+            </div>
+          );
+        }
         return (
           <div className={`${styles.item}`} key={v4()}>
             <Link href={`/memory/${feed.pk}`}>
