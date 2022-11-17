7b54ea2181bd7899c21da5a2d184d2ad1d54e3f1
Oh haengsong <haengsong@naver.com>
2022-11-17 16:57:20 +0900

    feat : 발행된거 없을때

--- front/components/memory/MemoryList.tsx
+++ front/components/memory/MemoryList.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import { v4 } from "uuid";
 import { useSelector } from "react-redux";
 import styles from "./MemoryList.module.scss";
 import getFeed from "../../pages/api/feed/getFeed";
 import { RootState } from "../../redux/store";
+import images from "../../public/icons/images.svg";
 
 function MemoryList() {
   const [feeds, setFeeds] = useState<any>([]);
@@ -48,38 +50,61 @@ function MemoryList() {
   // ];
   console.log(feeds);
   return (
-    <div className={`${styles.wrapper}`}>
-      {feeds.map((feed: { pk: any; feedId: any; feedImg: any }) => {
-        if (feed.pk === 9999) {
-          return (
-            <div className={`${styles.item}`} key={v4()}>
-              <div>
-                <div
-                  className={`${styles.imgBox} flex column justify-center align-center`}
-                >
-                  <h1 className={`${styles.imgBox__content} notoBold fs-14`}>
-                    새로운 피드를
-                  </h1>
-                  <h1 className={`${styles.imgBox__content} notoBold fs-14`}>
-                    발행 중입니다.
-                  </h1>
+    <div>
+      {feeds.length > 0 ? (
+        <div className={`${styles.wrapper}`}>
+          {feeds.map((feed: { pk: any; feedId: any; feedImg: any }) => {
+            if (feed.pk === 9999) {
+              return (
+                <div className={`${styles.item}`} key={v4()}>
+                  <div>
+                    <div
+                      className={`${styles.imgBox} flex column justify-center align-center`}
+                    >
+                      <h1
+                        className={`${styles.imgBox__content} notoBold fs-14`}
+                      >
+                        새로운 피드를
+                      </h1>
+                      <h1
+                        className={`${styles.imgBox__content} notoBold fs-14`}
+                      >
+                        발행 중입니다.
+                      </h1>
+                    </div>
+                  </div>
                 </div>
+              );
+            }
+            return (
+              <div className={`${styles.item}`} key={v4()}>
+                <Link href={`/memory/${feed.pk}`}>
+                  <div
+                    className={`${styles.imgBox} flex justify-center align-center`}
+                  >
+                    <img
+                      className={`${styles.img}`}
+                      src={feed.feedImg}
+                      alt="#"
+                    />
+                  </div>
+                </Link>
               </div>
-            </div>
-          );
-        }
-        return (
-          <div className={`${styles.item}`} key={v4()}>
-            <Link href={`/memory/${feed.pk}`}>
-              <div
-                className={`${styles.imgBox} flex justify-center align-center`}
-              >
-                <img className={`${styles.img}`} src={feed.feedImg} alt="#" />
-              </div>
-            </Link>
+            );
+          })}
+        </div>
+      ) : (
+        <div
+          className={`${styles.empty} flex column justify-center align-center`}
+        >
+          <div className={`${styles.img}`}>
+            <Image src={images} alt="#" />
           </div>
-        );
-      })}
+          <h1 className={`${styles.text} notoBold fs-18`}>
+            발행된 사진이 없습니다.
+          </h1>
+        </div>
+      )}
     </div>
   );
 }
