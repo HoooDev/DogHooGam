f74dae569cac00d90383be40c1b78473e28085e3
Oh haengsong <haengsong@naver.com>
2022-11-17 14:58:02 +0900

    feat: 피드 뒤로가기버튼, 지도크기수정

--- front/pages/memory/[memoryPk].tsx
+++ front/pages/memory/[memoryPk].tsx
@@ -3,9 +3,12 @@ import { Map, MapMarker } from "react-kakao-maps-sdk";
 import { useRouter } from "next/router";
 // import Image from "next/image";
 import { useEffect, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
 import styles from "./[memoryPk].module.scss";
 // import testimg from "../../public/images/test.png";
 import getOneFeed from "../api/feed/getOneFeed";
+import back from "../../public/icons/back.svg";
 
 function Detail() {
   const [feed, setFeed] = useState<any>({});
@@ -44,13 +47,20 @@ function Detail() {
       {/* <h1 className={`${styles.Nav} fs-20 notoBold`}>추억 남기기</h1> */}
       <div className={`${styles.detail}`}>
         <div className={`${styles.imgBox}`}>
-          <button
-            className={`${styles.copyBtn}`}
-            type="button"
-            onClick={() => handleCopyClipBoard(feed.transactionHash)}
-          >
-            트랜잭션 해쉬 복사
-          </button>
+          <div className="flex justify-space-between">
+            <Link href="/memory">
+              <div className={`${styles.backicon}`}>
+                <Image src={back} alt="#" />
+              </div>
+            </Link>
+            <button
+              className={`${styles.copyBtn}`}
+              type="button"
+              onClick={() => handleCopyClipBoard(feed.transactionHash)}
+            >
+              트랜잭션 해쉬 복사
+            </button>
+          </div>
           <img className={`${styles.img}`} src={feed.feedImg} alt="#" />
         </div>
 
@@ -60,40 +70,42 @@ function Detail() {
         <h1 className={`${styles.time} fs-16 notoBold`}>{feed.createDate}</h1>
       </div>
       {feed?.pk && (
-        <Map // 지도를 표시할 Container
-          center={{
-            // 지도의 중심좌표
-            lat: feed.lat,
-            lng: feed.lng
-          }}
-          style={{
-            // 지도의 크기
-            width: "100%",
-            height: "450px"
-          }}
-          level={4} // 지도의 확대 레벨
-        >
-          <MapMarker // 마커를 생성합니다
-            position={{
-              // 마커가 표시될 위치입니다
+        <div className={`${styles.map}`}>
+          <Map // 지도를 표시할 Container
+            center={{
+              // 지도의 중심좌표
               lat: feed.lat,
               lng: feed.lng
             }}
-            image={{
-              src: "https://lab.ssafy.com/s07-final/S07P31C103/uploads/ba2d9ac3ceb1413dccda58b29d010819/pin.png", // 마커이미지의 주소입니다
-              size: {
-                width: 40,
-                height: 40
-              }, // 마커이미지의 크기입니다
-              options: {
-                offset: {
-                  x: 27,
-                  y: 69
-                } // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-              }
+            style={{
+              // 지도의 크기
+              width: "80%",
+              height: "250px"
             }}
-          />
-        </Map>
+            level={4} // 지도의 확대 레벨
+          >
+            <MapMarker // 마커를 생성합니다
+              position={{
+                // 마커가 표시될 위치입니다
+                lat: feed.lat,
+                lng: feed.lng
+              }}
+              image={{
+                src: "https://lab.ssafy.com/s07-final/S07P31C103/uploads/ba2d9ac3ceb1413dccda58b29d010819/pin.png", // 마커이미지의 주소입니다
+                size: {
+                  width: 40,
+                  height: 40
+                }, // 마커이미지의 크기입니다
+                options: {
+                  offset: {
+                    x: 27,
+                    y: 69
+                  } // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+                }
+              }}
+            />
+          </Map>
+        </div>
       )}
     </div>
   );
